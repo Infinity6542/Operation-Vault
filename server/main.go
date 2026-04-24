01155a1971b85bd55030164555391c32742555b0
James <isd6542@outlook.com>
2026-04-24 20:18:31 +1000

    feat(server -> cleanup): cancel rw in cleanup

--- server/main.go
+++ server/main.go
@@ -603,7 +603,8 @@ func cleanupLoop() error {
 					continue
 				}
 				logger.Infof("Removing inactive client %s from channel %s", peerID, channelID)
-				// client.Stream.CancelRead(0)
+				client.Stream.CancelRead(0)
+				client.Stream.CancelWrite(0)
 				delete(clients, peerID)
 				changed = true
 			}
